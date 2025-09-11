f3817355067767de501b822a5e567cc8df19fa95
FatimaKhaliil <154417953+FatimaKhaliil@users.noreply.github.com>
2025-09-11 21:23:24 +0200

    save changes in firebase

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyAv4BRWLaH-CtwB7fqWNQFtbu1Ir2WD7zU",
     authDomain: "wpa-anwendung.firebaseapp.com",
     projectId: "wpa-anwendung",
-    storageBucket: "wpa-anwendung.appspot.com",
+    storageBucket: "wpa-anwendung.firebasestorage.app",
     messagingSenderId: "64469726337",
     appId: "1:64469726337:web:f8ea4183429dabafc44980"
   };
